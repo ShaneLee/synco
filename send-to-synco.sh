ea893c1b38b14e203252048d44f220d6673bbca1
Shane <me@shanel.ee>
2024-08-10 14:02:39 +0100

    Allow generic syncing

--- send-to-synco.sh
+++ send-to-synco.sh
@@ -10,16 +10,17 @@ function books {
   local sync_file="$HOME/synco/books/books-synced"
   local server_sync_file="$HOME/synco/books/server-books-synced"
   local temp_dir="$HOME/synco/books/books-to-sync"
-  curl "http://192.168.0.46/synco/books/server-books-synced" > $server_sync_file
+  curl "http://$NIGHTINGALE/synco/books/server-books-synced" > $server_sync_file
   rm $sync_file
+  # Pull the mobi files from Calibre's library
   find "$library" -type f -name "*.mobi" | while read -r mobi_file; do
     filename=$(basename "$mobi_file")
-    # ln -s "$mobi_file" "$destination_dir/$filename"
-    # echo $filename
     echo $mobi_file >> $sync_file
   done
 
   mkdir -p $temp_dir
+  # Find the difference between what the server has or has had in the past and
+  # what we have, then send only those files
   diff $sync_file $server_sync_file | grep '^<' | sed 's/^< //' | while read -r mobi_file; do
     echo "$mobi_file"
     filename=$(basename "$mobi_file")
@@ -32,5 +33,14 @@ function books {
   rm -r $temp_dir
 }
 
-books
+function generic_sync {
+  local type=$1 # e.g audiobooks
+  # -a (archive): This option preserves the permissions, timestamps, symbolic links, and other file attributes.
+  # -v (verbose): This option shows the progress of the operation.
+  # --ignore-existing: This option ensures that existing files on the server will not be overwritten. Only files that don't exist on the server will be copied.
+  rsync -av --ignore-existing "$HOME/synco/$type/ shane@$NIGHTINGALE:~/synco/$type/
+}
 
+books
+generic_sync "audiobooks"
+generic_sync "music"
